3c5a771029e849647401a02885655a74d26b6d7e
Víctor Cruz <vmcruz16@gmail.com>
2018-05-29 09:47:52 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    res.render('maintenance');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    var now = (new Date()).toString();
+    fs.appendFile('server.log', `${now}: ${req.method} ${req.url}\n`, (e) => {
+        if(e)
+            console.log('Unable to appendFile to server.log');
+    });
+    next();
+});
+
+hbs.registerHelper('getCurrentYear', () => (new Date).getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        pageTitle: 'Home Page',
+        currentYear: (new Date).getFullYear(),
+        welcomeMessage: 'Welcome'
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        pageTitle: 'About Page',
+        currentYear: (new Date).getFullYear()
+    })
+});
+
+app.get('/bad', (req, res) => {
+    res.send({ errorMessage: 'Unable to fulfill this request'});
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000...');
+});
